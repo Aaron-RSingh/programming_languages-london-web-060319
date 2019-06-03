f5732fc5b74a31af9c617a3b2fc9fcb192a9354a
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-06-03 08:47:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,6 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {} # your code here
+  languages.each do |style, data|
+    data.each do |lang_name, describe|#lang_name:
+    
 end
